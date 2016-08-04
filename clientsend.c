85551e45ece3c4410387db224124e3999c5ab577
ksuteamwork <allasd900128@gmail.com>
2016-08-04 12:17:18 +0800

    add

--- clientsend.c
+++ clientsend.c
@@ -10,18 +10,19 @@
 #include<strings.h>
 
 #define PORT  80
-#define SERVER_IP "192.168.128.65"
+#define SERVER_IP "192.168.128.95"
 
 int main()
  {
-  int s;
+  int s,len=0;
   struct sockaddr_in addr;
+  //int addr_len = sizeof(struct sockaddr_in);
   char buffer[1024];
   char var[]="GET /1.txt HTTP/1.1\r\n"
-             "User-Agent:Wget/1.15.1(linux-gnu)\r\n"
-             "Accept:*/*\r\n"
+             "User-Agent: Wget/1.17.1(linux-gnu)\r\n"
+             "Accept: */*\r\n"
              "Accept-Encoding: identity\r\n"
-             "Host: 192.168.128.65\r\n"
+             "Host: 192.168.128.34\r\n"
              "Connection: Keep-Alive\r\n\r\n";
  // char buf[1024];
 
@@ -39,18 +40,26 @@ int main()
         perror("connect");
         exit(1);
       }
+
         // memset(buffer,0,sizeof(buffer));
           sprintf(buffer,"%s",var);
-          printf("%s\n",buffer);
+         // printf("%s\n",buffer);
 
-           write(s,buffer,sizeof(buffer));
+           write(s,buffer,strlen(buffer));
            bzero(buffer, sizeof(buffer));
-         recv(s, buffer, sizeof(buffer), 0);
-         printf("%s\n",buffer);
+          // read(s,buffer,sizeof(buffer));
+           recv(s, buffer, sizeof(buffer),0);
+           printf("%s\n",buffer);
+           bzero(buffer, sizeof(buffer));
+           recv(s, buffer, sizeof(buffer),0);
+           printf("%s\n",buffer);
+         //printf("%d\n",len);
+       //  FILE*fp = fopen("1.txt","r");
+         //while(fgets(buffer,1024,fp)!=NULL){
+         //write(s,buffer,sizeof(buffer));
 
-        // FILE*fp = fopen("1.txt","r");
-        // while(fgets(buffer,1024,fp)!=NULL){
-        // printf("%s\n",buffer);}
+         //read(s,buffer,sizeof(buffer));
+         //printf("%s\n",buffer);}
 
         //while(1){
           //  bzero(buffer, sizeof(buffer));
@@ -68,6 +77,6 @@ int main()
          // fclose(fp);
 
        // }
-       close(s);
+
   }
 
